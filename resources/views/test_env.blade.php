8277f7ae4ecbc3ee061b0bfc28e11f3adadf5cd1
DOMAIN\peter <peter777200067@gmail.com>
2024-02-23 14:53:33 +0800

    # 測試SHOW ENV

--- resources/views/test_env.blade.php
+++ resources/views/test_env.blade.php
@@ -1,3 +1,5 @@
 {{ getenv('TEST_SHOW') }}<br>
 {{ config('broadcasting.default') }}<br>
+{{ config('broadcasting.connections.pusher.driver') }}<br>
+{{ config('broadcasting.connections.pusher.key') }}<br>
 {{ config('app.env') }}
