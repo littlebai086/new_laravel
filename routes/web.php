32b13804e87c294f62f2d4f5854560a1e57793f1
littlebai086 <peter777200067@gmail.com>
2024-03-02 03:10:43 +0800

    # 測試

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+ //111
 Route::get('/test_env', function () {
     return view('test_env');
 });
